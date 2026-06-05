2ca350a83de0ccb9d49011ada608e24a38077d96
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-05 15:47:36 +0300

    Time: 0 ms (100%), Space: 20.5 MB (41.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        set1=set(nums)
-        for i in range(len(nums)+1):
-            if i not in set1:
-                return i
+        n=len(nums)
+        total=n/2*(1+n)
+        current=sum(nums)
+        return int(total-current)
+
         
