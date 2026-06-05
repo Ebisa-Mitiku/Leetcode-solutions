5579ad0bc639efd7aeb515536e4b5cf2498c74ef
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-05 13:08:25 +0300

    Time: 4 ms (80.69%), Space: 19.3 MB (18.42%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if(x<0):
+            return False
+        x=str(x)
+        y=x[::-1]
+        return y==x
+
+
+     
+        
