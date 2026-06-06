a2d0026384141456cb2c2730b62532568c962603
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-06 15:36:25 +0300

    Time: 3 ms (52.16%), Space: 19.2 MB (90.51%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,14 +1,27 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-       
-        s=sorted(s)
-        t=sorted(t)
-
-        for i in range(len(t)-1):
-            if  t[i]!=s[i]:
-                return t[i]
-        return t[-1]
+    
+        dics={}
+        dic={}
           
+        for i in s:
+            dics[i]=dics.get(i,0)+1
+        
+        for i in t:
+            dic[i]=dic.get(i,0)+1
+        
+        for i in dic:
+            if i not in dics:
+                return i
+
+            if dic[i]!=dics[i]:
+                return i
+
+       
+
+        
+
+           
 
 
 
