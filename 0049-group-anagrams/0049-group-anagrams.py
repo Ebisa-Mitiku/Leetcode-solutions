27a5225d1c8796fdd1ca9a25562f32d7f7f5a6bd
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-09 21:31:42 +0300

    Time: 11 ms (84.02%), Space: 22.2 MB (49.12%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,11 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         counter=defaultdict(list)
-        ans=[]
-
         for word in strs:
             temp="".join(sorted(word))
             counter[temp].append(word)
+        return(list(counter.values()))
 
-        for key in counter:
-            ans.append(counter[key])
-        return ans      
 
 
   
