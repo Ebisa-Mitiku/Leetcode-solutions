83dd55ca87ebb971b5ac6a8fd11fee27325c357d
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-16 15:07:33 +0300

    Time: 0 ms (100%), Space: 19.3 MB (21.7%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,12 +1,11 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        ans=""
-        count=zip(s,indices)
-        Sorted_count=sorted(count,key=lambda x:x[1])
-        for key,val in Sorted_count:
-            ans+=key
-        return ans
+        ans=['']*len(s)
 
+        for i,ch in enumerate(s):
+            ans[indices[i]]=ch
+        return "".join(ans)
+   
         
 
         
