cbc110de5a9441254e925f89dad36d76b4f86f52
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-19 13:36:47 +0300

    Time: 27 ms (30.41%), Space: 19.7 MB (25.37%) - LeetHub

--- 2404-most-frequent-even-element/2404-most-frequent-even-element.py
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -2,20 +2,18 @@ class Solution:
     def mostFrequentEven(self, nums: List[int]) -> int:
         nums=Counter(nums)
         mini=0
+        ans=-1
 
-        even=[]
         for key in nums:
             if key%2==0:
                 if nums[key]>mini:
                     mini=nums[key]
+                    ans=key
+                elif nums[key]==mini and key<ans:
+                    ans=key
         
-        for key in nums:
-            if key%2==0 and nums[key]==mini:
-                even.append(key)
-        
-        if even:
-            return min(even)
-        return -1
+        return ans
+
 
                 
         
