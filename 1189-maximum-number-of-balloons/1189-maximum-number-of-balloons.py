1f675966ea1cc2b2afceee868174c14da5a945e0
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-22 17:41:11 +0300

    Time: 3 ms (61.87%), Space: 19.4 MB (38.08%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,25 +1,10 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-       
-        s={"b","a","l","o","n"}
-        dic={}
-        for i in range(len(text)):
-            if text[i] in s:
-                dic[text[i]]=dic.get(text[i],0)+1
-
-
-        if len(set(dic))<5:
-            return 0
-        limiter=float("inf")
-        for key in dic:
-            if key in s:
-                if key=="o" or key=="l":
-                    limiter=min(limiter,dic[key]//2)
-                else:
-                    limiter=min(limiter,dic[key])
-        return limiter
-
-        
-
-
-        
+        count=Counter(text)
+        return min(
+            count.get("b", 0),
+            count.get("a", 0),
+            count.get("l", 0) // 2,
+            count.get("o", 0) // 2,
+            count.get("n", 0)
+        )
