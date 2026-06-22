7ab3b5594ac8d24117d284c3331c1b87a7bee660
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-22 17:47:17 +0300

    Time: 0 ms (100%), Space: 19.3 MB (74.13%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.py
@@ -1,9 +1,11 @@
 class Solution:
     def rearrangeCharacters(self, s: str, target: str) -> int:
         count=Counter(s)
+        t_count=Counter(target)
 
         limit=float('inf')
 
         for ch in target:
-            limit=min(limit,count[ch]//target.count(ch))
+            limit=min(limit,count[ch]//t_count[ch])
+            
         return limit       
