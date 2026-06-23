6ba6454ab7e2974b4fb7a7ac2780ae6c43798a7d
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-23 12:43:45 +0300

    Time: 4 ms (37.5%), Space: 19.3 MB (57.41%) - LeetHub

--- 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.py
@@ -1,15 +1,9 @@
 class Solution:
     def greatestLetter(self, s: str) -> str:
         word=set(s)
-        ans=[]
+        ans=""
 
         for ch in word:
-            if ch.islower():
-                uc=ch.upper()
-                if uc in word:
-                    ans.append(uc)
-
-        if ans:
-            return max(ans)
-        return ""
-        
+            if ch.isupper() and ch.lower() in word:
+                ans=max(ans,ch)
+        return ans
