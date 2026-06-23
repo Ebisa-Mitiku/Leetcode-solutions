db2b970964fe2d2d6e75318e081bab7b3a05881f
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-23 11:29:23 +0300

    Time: 0 ms (100%), Space: 19.2 MB (90.12%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,9 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+      
+        return (
+            word.upper()==word or 
+            word.lower()==word or 
+            word[0].upper()+word[1:].lower()==word
+        )
+        
