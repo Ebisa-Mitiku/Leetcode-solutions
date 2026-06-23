fa52c3437f71cf2cc542ed661b6f7323acc10c01
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-23 12:08:54 +0300

    Time: 0 ms (100%), Space: 19.3 MB (45.39%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.py
+++ 0709-to-lower-case/0709-to-lower-case.py
@@ -1,11 +1,4 @@
 class Solution:
     def toLowerCase(self, s: str) -> str:
-
-        ans=""
-        for i in s:
-            if 65<=ord(i)<=90:
-                ans+=chr(ord(i)+32)
-            else:
-                ans+=i
-        return ans
+        return s.lower()
        
