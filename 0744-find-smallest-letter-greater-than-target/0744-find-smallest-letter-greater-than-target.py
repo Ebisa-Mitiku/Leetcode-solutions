fa7d9f5b8a99fbd564be445c2c32dabe8444c815
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-24 17:16:32 +0300

    Time: 0 ms (100%), Space: 20.6 MB (50.95%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.py
@@ -0,0 +1,10 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+
+        for ch in letters:
+            if ch>target:
+                return ch
+
+        return letters[0]
+
+        
