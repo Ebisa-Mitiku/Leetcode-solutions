7c6f899e0107a2a47c264292f377c480c30900dc
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-06-27 12:41:10 +0300

    Time: 21 ms (49.23%), Space: 20.4 MB (41.86%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.py
@@ -4,18 +4,7 @@ class Solution:
             return prod(nums)
 
         nums.sort()
-        neg=0
-
-        for i in range(len(nums)):
-            if nums[i]<0:
-                neg+=1
-
-        if neg<2:
-            return nums[-1]*nums[-2]*nums[-3]
-
-        x=nums[-1]*nums[-2]*nums[-3]
-        y=nums[0]*nums[1]*nums[-1]
-        return max(x,y)
+        return max(nums[-1]*nums[-2]*nums[-3],nums[0]*nums[1]*nums[-1])
 
         
 
