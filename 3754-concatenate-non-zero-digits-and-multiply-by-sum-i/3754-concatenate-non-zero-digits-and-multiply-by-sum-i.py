b4319df6ba1fa76f731c3333732ad634b35b08ac
Ebisa-Mitiku <ebisamitiku203@gmail.com>
2026-07-07 10:44:11 +0300

    Time: 3 ms (22.16%), Space: 19.3 MB (51.25%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.py
@@ -1,18 +1,9 @@
 class Solution:
     def sumAndMultiply(self, n: int) -> int:
-        x=""
+        x="".join(d for d in str(n) if d!="0")
 
-        for num in str(n):
-            if num!="0":
-                x+=num
-                
         if not x:
             return 0
 
-        ans=0
-        for i in x:
-            ans+=int(i)
-
-
-        return(int(x)*ans)
+        return(int(x)*sum(map(int,x)))
         
